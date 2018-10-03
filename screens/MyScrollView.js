9a31ed85dc3029c28cb48ee07b0efb4733148c56
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-10-03 14:56:15 +0800

    scroll view

---
+++ screens/MyScrollView.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {AppRegistry, ScrollView, View, Text, Image} from 'react-native';
+
+export default class MyScrollView extends Component {
+
+    render() {
+        return (
+            <ScrollView >
+                <View>
+                    <Text style={{fontSize: 96}}>Scroll me plz</Text>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                    <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png",width:60,height:100,}}/>
+                </View>
+            </ScrollView>
+        )
+    }
+
+}
+
+AppRegistry.registerComponent("AwesomeProject",()=>MyScrollView);
